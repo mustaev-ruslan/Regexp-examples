b44d696c4bb806f71cf8645b9d35bac92d903425
Ruslan Mustaev <mustaev.ruslan@hotmail.com>
2018-01-21 04:27:06 +0300

    Refactoring: delete Patterns, crate patternsMap

--- src/main/java/regexp/RegexpHelper.java
+++ src/main/java/regexp/RegexpHelper.java
@@ -1,10 +1,70 @@
 package regexp;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.*;
 
 public class RegexpHelper {
+
+    private static Map<String, String> strMap;
+    private static Map<String, Pattern> patternMap;
+
+    static {
+        strMap = new HashMap<>();
+        initStrMap();
+        patternMap = new HashMap<>();
+        for (Map.Entry<String, String> entry : strMap.entrySet()) {
+            patternMap.put(entry.getKey(), Pattern.compile(entry.getValue()));
+        }
+    }
+
+    private static void initStrMap() {
+        // Почта
+        strMap.put("MAIL", "[a-zA-Z]([.-]?[a-zA-`Z0-9_]+)*@[a-zA-Z]([.-]?[a-zA-Z0-9_]+)*");
+        // То что может быть именем и фамилией, начинаются с большой буквы, не содержат небуквенных символов,
+        // разделены минимум одним пробелом
+        strMap.put("NAME_SURRNAME", "");
+        // Адрес веб-странички http://host/address
+        strMap.put("PAGE_ADDRESS", "");
+        // Цифровые часы. 10:55
+        strMap.put("DIGITAL_CLOCK", "");
+        // IP-адрес в десятичном представлении. 127.0.0.1
+        strMap.put("IP", "");
+        // Жадные круглые скобки. Возвращает любые скобочные выражения.
+        strMap.put("GREEDY_BRACKETS", "");
+        // Ленивые круглые скобки. Возвращает скобочные выражения без содержания внутри правильной скобочной последовательности.
+        strMap.put("LAZY_BRACKETS", "");
+        // Самые большие круглые скобки. Возращает только самые большие скобочные выражения. Не возвращает подскобочные скобочные выражения.
+        strMap.put("BIG_BRACKETS", "");
+        // Российский номер автомобиля
+        strMap.put("AUTO_NUMBER", "");
+        // Слова с удвоенной "н"
+        strMap.put("DOUBLE_N", "");
+        // Русские прилагательные. -ая, -ое, -ие, -яя, ...
+        strMap.put("ADJECTIVE", "");
+        // Числовое равенство. 3 + 5 = 7
+        strMap.put("NUM_EQUALITY", "");
+        // Телефонный номер со скобками и дефисами или без
+        strMap.put("PHONE", "");
+        // Красное или зеленое яблоко
+        strMap.put("APPLE", "");
+        // Шестнадцатиричное число без впереди идущих нулей
+        strMap.put("HEX", "");
+        // Любое натуральное число
+        strMap.put("NATURAL", "");
+        // Любое рациональное число
+        strMap.put("RATIO", "");
+        // Кот, но не котел и не икота
+        strMap.put("CAT", "");
+        // Ко...т - слово целиком
+        strMap.put("CA_T", "");
+        // Дата в любом формате
+        strMap.put("DATE", "");
+        // Предложение, в котором встречается повтор одного слова(возможно в разном регистре) и вхождения этого слова
+        // разделены минимум одним пробельным символом.
+        strMap.put("DOUBLE_WORD_SENTENCE", "");
+        // Слова вида аааббб, где кол-во а и б одинаково
+        strMap.put("AAABBB", "");
+    }
 
     public static boolean isMatches(String text, Pattern pattern) {
         if (text == null || text.isEmpty()) {
@@ -23,5 +83,9 @@ public class RegexpHelper {
             matchesSet.add(m.group());
         }
         return matchesSet;
+    }
+
+    public static Pattern getPattern(String patternName) {
+        return patternMap.get(patternName);
     }
 }
--- src/test/java/RegexpHelperTests.java
+++ src/test/java/RegexpHelperTests.java
@@ -1,6 +1,5 @@
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import regexp.Patterns;
 import regexp.RegexpHelper;
 
 import java.util.*;
@@ -18,58 +17,58 @@ public class RegexpHelperTests {
     @DataProvider
     public Object[][] isMatchesData() {
         return new Object[][]{
-                {Patterns.MAIL, null, false},
-                {Patterns.MAIL, "", false},
-                {Patterns.MAIL, "@", false},
-                {Patterns.MAIL, "a@", false},
-                {Patterns.MAIL, "a@@a", false},
-                {Patterns.MAIL, "@a", false},
-                {Patterns.MAIL, "a!a@a", false},
-                {Patterns.MAIL, "a%@a", false},
-                {Patterns.MAIL, "y@df!", false},
-                {Patterns.MAIL, "1@a", false},
-                {Patterns.MAIL, ".@a", false},
-                {Patterns.MAIL, "a.@a", false},
-                {Patterns.MAIL, ".a@a", false},
-                {Patterns.MAIL, "y@1", false},
-                {Patterns.MAIL, "y@1as", false},
-                {Patterns.MAIL, "-y@s", false},
-                {Patterns.MAIL, "y-@a", false},
-                {Patterns.MAIL, "y..a@a", false},
-                {Patterns.MAIL, "a@a..a", false},
-                {Patterns.MAIL, "y-a@a", true},
-                {Patterns.MAIL, "a@a-a", true},
-                {Patterns.MAIL, "a@a", true},
-                {Patterns.MAIL, "A@B", true},
-                {Patterns.MAIL, "a12df@a12", true},
-                {Patterns.MAIL, "a@a.ru", true},
-                {Patterns.MAIL, "a.b@a", true},
-                {Patterns.MAIL, "mail______aaa@a.ru.rt", true},
+                {"MAIL", null, false},
+                {"MAIL", "", false},
+                {"MAIL", "@", false},
+                {"MAIL", "a@", false},
+                {"MAIL", "a@@a", false},
+                {"MAIL", "@a", false},
+                {"MAIL", "a!a@a", false},
+                {"MAIL", "a%@a", false},
+                {"MAIL", "y@df!", false},
+                {"MAIL", "1@a", false},
+                {"MAIL", ".@a", false},
+                {"MAIL", "a.@a", false},
+                {"MAIL", ".a@a", false},
+                {"MAIL", "y@1", false},
+                {"MAIL", "y@1as", false},
+                {"MAIL", "-y@s", false},
+                {"MAIL", "y-@a", false},
+                {"MAIL", "y..a@a", false},
+                {"MAIL", "a@a..a", false},
+                {"MAIL", "y-a@a", true},
+                {"MAIL", "a@a-a", true},
+                {"MAIL", "a@a", true},
+                {"MAIL", "A@B", true},
+                {"MAIL", "a12df@a12", true},
+                {"MAIL", "a@a.ru", true},
+                {"MAIL", "a.b@a", true},
+                {"MAIL", "mail______aaa@a.ru.rt", true},
         };
     }
 
     @Test(dataProvider = "isMatchesData")
-    public void testIsMatch(Patterns pattern, String text, boolean expected) {
-        final boolean actual = RegexpHelper.isMatches(text, pattern.getPattern());
+    public void testIsMatch(String patternName, String text, boolean expected) {
+        final boolean actual = RegexpHelper.isMatches(text, RegexpHelper.getPattern(patternName));
         assertEquals(actual, expected, "mail: " + text);
     }
 
     @DataProvider
     public static Object[][] findUniqueMatchesData() {
         return new Object[][]{
-                {Patterns.MAIL, null, Set()},
-                {Patterns.MAIL, "", Set()},
-                {Patterns.MAIL, "@", Set()},
-                {Patterns.MAIL, "Hello world!", Set()},
-                {Patterns.MAIL, "Hello@ @hello", Set()},
-                {Patterns.MAIL, "a@A", Set("a@A")},
-                {Patterns.MAIL, "Hello, my name is Ruslan, my mail is: ruslan1989-mustaev@mail.ru",
+                {"MAIL", null, Set()},
+                {"MAIL", "", Set()},
+                {"MAIL", "@", Set()},
+                {"MAIL", "Hello world!", Set()},
+                {"MAIL", "Hello@ @hello", Set()},
+                {"MAIL", "a@A", Set("a@A")},
+                {"MAIL", "Hello, my name is Ruslan, my mail is: ruslan1989-mustaev@mail.ru",
                         Set("ruslan1989-mustaev@mail.ru")
                 },
-                {Patterns.MAIL, "This is list of any mails: hello@my, no,no, adid@b.r, aga#jl, afa@no, ha@, @ga, no@no.no.n.ru, hello@my",
+                {"MAIL", "This is list of any mails: hello@my, no,no, adid@b.r, aga#jl, afa@no, ha@, @ga, no@no.no.n.ru, hello@my",
                         Set("adid@b.r", "hello@my", "afa@no", "no@no.no.n.ru", "hello@my")
                 },
-                {Patterns.MAIL, "This is doubling of any mails: hello@my, hello@my, hello@my, no,no, adid@b.r, aga#jl, afa@no, afa@no," +
+                {"MAIL", "This is doubling of any mails: hello@my, hello@my, hello@my, no,no, adid@b.r, aga#jl, afa@no, afa@no," +
                         " ha@, @ga, no@no.no.n.ru, hello@my",
                         Set("hello@my", "adid@b.r", "afa@no", "no@no.no.n.ru", "hello@my")
                 },
@@ -77,8 +76,8 @@ public class RegexpHelperTests {
     }
 
     @Test(dataProvider = "findUniqueMatchesData")
-    public void testFindUniqueMatches(Patterns pattern, String text, Set<String> expectedSet) {
-        final Set<String> actualSet = RegexpHelper.findUniqueMatches(text, pattern.getPattern());
+    public void testFindUniqueMatches(String patternName, String text, Set<String> expectedSet) {
+        final Set<String> actualSet = RegexpHelper.findUniqueMatches(text, RegexpHelper.getPattern(patternName));
         assertEquals(actualSet, expectedSet, "text: " + text);
     }
 }
